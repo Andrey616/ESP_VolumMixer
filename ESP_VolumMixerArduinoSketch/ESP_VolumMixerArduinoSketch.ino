5236efd8f24eed7d22f62bf2b58aa1c12c5400c5
Andrey616 <andrey.cheban576@gmail.com>
2026-01-16 22:26:09 +0300

    Update Read Potenseometrs

--- ESP_VolumMixerArduinoSketch/ESP_VolumMixerArduinoSketch.ino
+++ ESP_VolumMixerArduinoSketch/ESP_VolumMixerArduinoSketch.ino
@@ -1,7 +1,6 @@
 const int Potenseometrs[] = {A0, A1, A2, A3, A7};
 const int PotenseometrCount = sizeof(Potenseometrs) / sizeof(Potenseometrs[0]);
 
-
 int PotenseometrsValueOld[] = {0, 0, 0, 0, 0};
 int PotenseometrsValueNev[] = {0, 0, 0, 0, 0};
 
@@ -15,25 +14,26 @@ void setup() {
 
 }
 
-void ReadPotenseometrs(){
+String ReadPotenseometrs(){
+  String RezulrReadValuePotenseometrs = "";
   for (int PotenseometrPin = 0; PotenseometrPin < PotenseometrCount - 1; PotenseometrPin++) {
       PotenseometrsValueNev[PotenseometrPin] = map(analogRead(Potenseometrs[PotenseometrPin]), 16, 1023, 0, 100);
       if (PotenseometrsValueNev[PotenseometrPin] != PotenseometrsValueOld[PotenseometrPin]){
         PotenseometrsValueOld[PotenseometrPin] = PotenseometrsValueNev[PotenseometrPin];
-
-        Serial.print("Значение потенциометра: "); Serial.print(PotenseometrPin); Serial.print(" "); Serial.println(PotenseometrsValueOld[PotenseometrPin]);
       }
+      RezulrReadValuePotenseometrs += String(PotenseometrsValueOld[PotenseometrPin]) + "| ";
     }
     PotenseometrsValueNev[PotenseometrCount-1] = map(analogRead(Potenseometrs[PotenseometrCount-1]), 0, 1023, 0, 100);
     if (PotenseometrsValueNev[PotenseometrCount-1] != PotenseometrsValueOld[PotenseometrCount-1]){
       PotenseometrsValueOld[PotenseometrCount-1] = PotenseometrsValueNev[PotenseometrCount-1];
-      Serial.print("Значение потенциометра: "); Serial.print(PotenseometrCount-1); Serial.print(" "); Serial.println(PotenseometrsValueOld[PotenseometrCount-1]);
     }
+    RezulrReadValuePotenseometrs += String(PotenseometrsValueOld[PotenseometrCount-1]) + "| ";
+    return (RezulrReadValuePotenseometrs);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  ReadPotenseometrs();
+  //Serial.println(ReadPotenseometrs()); 
   
 
 
