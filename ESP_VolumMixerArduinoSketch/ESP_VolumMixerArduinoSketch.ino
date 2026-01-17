aea43fcb855c9c6ed611707f9386a195c1d35fba
Andrey616 <andrey.cheban576@gmail.com>
2026-01-17 13:28:45 +0300

    Appand Read Encoders

--- ESP_VolumMixerArduinoSketch/ESP_VolumMixerArduinoSketch.ino
+++ ESP_VolumMixerArduinoSketch/ESP_VolumMixerArduinoSketch.ino
@@ -1,8 +1,18 @@
+#include <EncButton.h>
+
 const int Potenseometrs[] = {A0, A1, A2, A3, A7};
 const int PotenseometrCount = sizeof(Potenseometrs) / sizeof(Potenseometrs[0]);
 
+const int Encoders[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, A6};
+const int EncoderCount = sizeof(Encoders) / sizeof(Encoders[0]);
+
 int PotenseometrsValueOld[] = {0, 0, 0, 0, 0};
 int PotenseometrsValueNev[] = {0, 0, 0, 0, 0};
+int EncoderValue[] = {0, 0, 0, 0};
+
+
+
+EncButton* encoderObjects[EncoderCount / 3]; 
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,6 +20,9 @@ void setup() {
 
   for (int PotenseometrPin = 0; PotenseometrPin < PotenseometrCount; PotenseometrPin++) {
     pinMode(Potenseometrs[PotenseometrPin], INPUT_PULLUP);
+  }
+  for (int EncoderPin = 0; EncoderPin < EncoderCount / 3; EncoderPin++) {
+    encoderObjects[EncoderPin] = new EncButton(Encoders[EncoderPin*3], Encoders[EncoderPin*3 + 1], Encoders[EncoderPin*3 + 2]);
   }
 
 }
@@ -28,12 +41,44 @@ String ReadPotenseometrs(){
       PotenseometrsValueOld[PotenseometrCount-1] = PotenseometrsValueNev[PotenseometrCount-1];
     }
     RezulrReadValuePotenseometrs += String(PotenseometrsValueOld[PotenseometrCount-1]) + "| ";
-    return (RezulrReadValuePotenseometrs);
+    return RezulrReadValuePotenseometrs;
+}
+
+String ReadEncoders(){
+  String RezulrReadValueEncoders = "";
+  for (int EncoderObgectPosition = 0; EncoderObgectPosition < EncoderCount / 3; EncoderObgectPosition++) {
+    encoderObjects[EncoderObgectPosition]->tick();
+
+    if (encoderObjects[EncoderObgectPosition]->right()) {
+      EncoderValue[EncoderObgectPosition] += 1;
+    }
+
+    if (encoderObjects[EncoderObgectPosition]->left()) {
+      EncoderValue[EncoderObgectPosition] -= 1;
+    }
+
+    if (encoderObjects[EncoderObgectPosition]->rightH()) {
+      EncoderValue[EncoderObgectPosition] += 5;
+    }
+
+    if (encoderObjects[EncoderObgectPosition]->leftH()) {
+      EncoderValue[EncoderObgectPosition] -= 5;
+    }
+
+    // Ограничиваем значение (опционально)
+    EncoderValue[EncoderObgectPosition] = constrain(EncoderValue[EncoderObgectPosition], 0, 100);
+    
+    RezulrReadValueEncoders += String(EncoderValue[EncoderObgectPosition]) + "| ";
+  }
+  return RezulrReadValueEncoders;  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //Serial.println(ReadPotenseometrs()); 
+  //Serial.print(ReadPotenseometrs()); 
+  //Serial.println(ReadEncoders());
+  
   
 
 
